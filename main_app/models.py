639fbfa3c4963cd6313824c4acc79572f08e2841
sara-searson <sara.r.searson@gmail.com>
2024-12-05 15:56:48 -0500

    added front end CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,4 +10,7 @@ class Cat(models.Model):
     age = models.IntegerField()
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("cat_detail", kwargs={ "cat_id": self.id })
     
